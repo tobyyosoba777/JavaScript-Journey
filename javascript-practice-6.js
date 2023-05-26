99625dd6e5151f53c3979b07b41460114c6ca652
tobyyosoba777 <tobiosoba777@gmail.com>
2023-05-26 15:47:35 -0500

    updated

--- javascript-practice-6.js
+++ javascript-practice-6.js
@@ -57,7 +57,7 @@ class user {
         document.writeln(this.name + " " + this.number);
     }
 }
-var opp = new user("tobi", 123456789101112131415617181920);
+var opp = new user("tobi", 12345678910111213141561718192021);
 opp.detail();
 
 document.write("<br>")
